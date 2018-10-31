6d7d7662263f09e09baaa7934d284bbe0d79f218
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-10-31 15:38:38 +0200

    add mongodb calls to request

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+import os, json, random
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+from bson import json_util
+
+env_mongo_uri = os.getenv('MONGODB_URI')
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = env_mongo_uri
+mongo = PyMongo(app)
+
+names = ['ruan', 'stefan']
 
 @app.route('/')
 def main():
-    return ''
+    data = mongo.db.people.find_one({"name": random.choice(names)})
+    return json.dumps(data, default=json_util.default)
 
 if __name__ == '__main__':
     app.run()
